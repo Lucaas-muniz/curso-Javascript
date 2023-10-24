260cb101ed23333d3e4f74b282d3c37b85679cac
Lucaas-muniz <luckasmuniz@gmail.com>
2023-10-24 10:53:27 -0300

    inicio das aulas sobre tipos de dados

---
+++ tipos de dados e operadores/aulas/1_numbers/script.js
@@ -0,0 +1,3 @@
+console.log(typeof 5);
+console.log(typeof -123);
+console.log(typeof 1.55);
---
+++ tipos de dados e operadores/aulas/2_operacaoAritmetica/script.js
@@ -0,0 +1,6 @@
+console.log(typeof (5+5));
+console.log(typeof (-123-100));
+console.log(typeof (1.55*2));
+console.log(typeof (10/2));
+console.log(typeof (20%2));
+console.log(typeof (21%2));
